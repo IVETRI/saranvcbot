e70a0a91b23a26ccfda37e26bf74db13b6fca696
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-07-24 17:52:36 +0530

    Update userbotjoin.py

--- VCsMusicBot/modules/userbotjoin.py
+++ VCsMusicBot/modules/userbotjoin.py
@@ -5,7 +5,7 @@ from VCsMusicBot.helpers.decorators import authorized_users_only, errors
 from VCsMusicBot.services.callsmusic.callsmusic import client as USER
 from VCsMusicBot.config import SUDO_USERS
 
-@Client.on_message(filters.command(["userbotjoin"]) & ~filters.private & ~filters.bot)
+@Client.on_message(filters.command(["join"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
 async def addchannel(client, message):
@@ -42,7 +42,7 @@ async def addchannel(client, message):
     )
 
 
-@USER.on_message(filters.group & filters.command(["userbotleave"]))
+@USER.on_message(filters.group & filters.command(["leave"]))
 @authorized_users_only
 async def rem(USER, message):
     try:
@@ -54,7 +54,7 @@ async def rem(USER, message):
         )
         return
     
-@Client.on_message(filters.command(["userbotleaveall"]))
+@Client.on_message(filters.command(["leaveall"]))
 async def bye(client, message):
     if message.from_user.id in SUDO_USERS:
         left=0
@@ -72,7 +72,7 @@ async def bye(client, message):
         await client.send_message(message.chat.id, f"Left {left} chats. Failed {failed} chats.")
     
     
-@Client.on_message(filters.command(["userbotjoinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
+@Client.on_message(filters.command(["joinchannel","ubjoinc"]) & ~filters.private & ~filters.bot)
 @authorized_users_only
 @errors
 async def addcchannel(client, message):
